24fc55796265ecdc44a0147858b12cacd9fa7359
Rodolfo Viana <rodolfo.viana@tvglobo.com.br>
2020-01-21 10:32:25 -0300

    Added IPCA-15; fixed typo in README

--- calculadora_do_cidadao/adapters/__init__.py
+++ calculadora_do_cidadao/adapters/__init__.py
@@ -1,2 +1,3 @@
 from calculadora_do_cidadao.adapters.ipca import Ipca  # noqa
 from calculadora_do_cidadao.adapters.selic import Selic  # noqa
+from calculadora_do_cidadao.adapters.ipca15 import Ipca15  # noqa

29b8b8453b8543d3d35b9b1cfaff082196091965
Rodolfo Viana <rodolfo.viana@tvglobo.com.br>
2020-01-23 13:32:55 -0300

    Reduced lines

--- calculadora_do_cidadao/adapters/__init__.py
+++ calculadora_do_cidadao/adapters/__init__.py
@@ -1,3 +1,2 @@
-from calculadora_do_cidadao.adapters.ipca import Ipca  # noqa
-from calculadora_do_cidadao.adapters.ipca import Ipca15  # noqa
+from calculadora_do_cidadao.adapters.ipca import Ipca, Ipca15  # noqa
 from calculadora_do_cidadao.adapters.selic import Selic  # noqa

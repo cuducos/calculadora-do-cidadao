82d3a980086ac48367782dab73b36a5c1a1afffa
Vinicius Mesel <vinicius@bigsp.com.br>
2020-02-12 19:59:30 -0300

    Fix little issues in the CPI Calculator, such as names, file API and other stuff

--- calculadora_do_cidadao/adapters/cpi.py
+++ calculadora_do_cidadao/adapters/cpi.py
@@ -10,56 +10,15 @@ from calculadora_do_cidadao.months import MONTHS
 from calculadora_do_cidadao.typing import MaybeIndexesGenerator
 
 
-today_date = str(date.today())
-
 URL = "https://fred.stlouisfed.org/graph/fredgraph.xls"
 URL_PARAMS = {
-    "bgcolor": "%23e1e9f0",
-    "chart_type": "line",
-    "drp": "0",
-    "fo": "open%20sans",
-    "graph_bgcolor": "%23ffffff",
-    "height": "450",
-    "mode": "fred",
-    "recession_bars": "on",
-    "txtcolor": "%23444444",
-    "ts": "12",
-    "tts": "12",
-    "width": "1168",
-    "nt": "0",
-    "thu": "0",
-    "trc": "0",
-    "show_legend": "yes",
-    "show_axis_titles": "yes",
-    "show_tooltip": "yes",
-    "id": "CPIAUCSL",
-    "scale": "left",
-    "cosd": "1947-01-01",
-    "coed": "2019-12-01",
-    "line_color": "%234572a7",
-    "link_values": "false",
-    "line_style": "solid",
-    "mark_type": "none",
-    "mw": "3",
-    "lw": "2",
-    "ost": "99999",
-    "oet": "99999",
-    "mma": "0",
-    "fml": "a",
-    "fq": "Monthly",
-    "fam": "avg",
-    "fgst": "lin",
-    "fgsnd": "2009-06-01",
-    "line_index": "1",
-    "transformation": "lin",
-    "vintage_date": today_date,
-    "revision_date": today_date,
-    "nd": "1947-01-01",
+    "id": "CpiAllUrbanCityAverage",
 }
 
 
-class Cpiaucsl(Adapter):
-    """Adapter for CPIAUCSL's FED's series."""
+class CpiAllUrbanCityAverage(Adapter):
+    """Adapter for FED's Consumer Price Index for
+    All Urban Consumers: All Items (CpiAllUrbanCityAverage)"""
 
     file_type = "xls"
 

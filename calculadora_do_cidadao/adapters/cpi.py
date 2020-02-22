7264ce69a5d71d0a73dd5029b3c1744373b25f78
Eduardo Cuducos <cuducos@users.noreply.github.com>
2020-02-22 12:09:12 -0500

    Refactors CPI adapter

--- calculadora_do_cidadao/adapters/cpi.py
+++ calculadora_do_cidadao/adapters/cpi.py
@@ -1,7 +1,7 @@
 from typing import NamedTuple
 from urllib.parse import urlencode
 
-from rows.fields import DateField, DecimalField
+from rows.fields import DecimalField
 
 from calculadora_do_cidadao.adapters import Adapter
 from calculadora_do_cidadao.typing import MaybeIndexesGenerator
@@ -18,17 +18,9 @@ class AllUrbanCityAverage(Adapter):
     file_type = "xls"
     url = f"{URL}?{urlencode(URL_PARAMS)}"
 
+    IMPORT_KWARGS = {"start_row": 10, "force_types": {"cpiaucsl": DecimalField}}
+
     def serialize(self, row: NamedTuple) -> MaybeIndexesGenerator:
-        """Serialize method to discard the rows that are not valid data."""
+        """Serialize method to unpack Rows's row into a tuple."""
         reference, value = row
-
-        try:
-            reference = DateField.deserialize(reference)
-            value = DecimalField.deserialize(value)
-        except ValueError:
-            return
-
-        if reference is None or value is None:
-            return
-
         yield reference, value

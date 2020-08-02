0bb2deb85db30adb1c24f36c289fcaf3c9ce3a63
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-08-01 21:37:58 -0300

    Add multiple possible input formats to DateField

--- calculadora_do_cidadao/fields.py
+++ calculadora_do_cidadao/fields.py
@@ -1,3 +1,4 @@
+import datetime
 from decimal import Decimal
 
 from rows import fields
@@ -14,6 +15,20 @@ class PercentField(fields.PercentField):
 
 
 class DateField(fields.DateField):
-    """Field for mmm/yyyy dates."""
+    """DateField which supports different date formats, including Brazilian"""
 
-    INPUT_FORMAT = "%b/%Y"
+    INPUT_FORMATS = ("%Y-%m-%d", "%d/%m/%Y", "%Y-%m", "%m/%Y", "%b/%Y", "%Y")
+
+    @classmethod
+    def deserialize(cls, value, *args, **kwargs):
+        value = fields.Field.deserialize(value)
+        value = fields.as_string(value)
+
+        for date_format in cls.INPUT_FORMATS:
+            try:
+                dt_object = datetime.datetime.strptime(value, date_format)
+            except ValueError:
+                continue
+            else:
+                return datetime.date(dt_object.year, dt_object.month, dt_object.day)
+        raise ValueError("Cannot parse value as date: {}".format(value))

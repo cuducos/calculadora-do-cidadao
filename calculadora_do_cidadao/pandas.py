0c6e4aa8d94bc955523e4c051e91128fc9606969
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-08-10 20:46:39 -0400

    WIP Creates Pandas wrapper for adpaters

--- calculadora_do_cidadao/pandas.py
+++ calculadora_do_cidadao/pandas.py
@@ -1,13 +1,17 @@
+from decimal import Decimal
+
+from pandas import Series
+
 from calculadora_do_cidadao.adapters import ibge
 
 
 class PandasWrapper:
     def __init__(self, adapter):
         self.adapter = adapter()
-        adapter.adjust = self.adjust
 
     def adjust(self, original_date, value=None, target_date=None):
-        raise NotImplementedError
+        # TODO: check if Pandas has a decimal/precision type
+        return original_date.apply(self.adapter.adjust)
 
     @classmethod
     def wrap(cls, adapter):
--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,16 +1,28 @@
 from decimal import Decimal
 from unittest import TestCase
+from unittest.mock import patch
 
 from pandas import Series
+from pandas.testing import assert_series_equal
 
+from calculadora_do_cidadao import Ipca as OriginalIpca
 from calculadora_do_cidadao.pandas import Ipca
+from tests import get_fixture
 
 
 class TestPandasUseCase(TestCase):
-    def test_one_argument(self):
+    @staticmethod
+    def assert_decimal_series_equal(left, right, **kwargs):
+        return assert_series_equal(left.astype(float), right.astype(float), **kwargs)
+
+    @patch("calculadora_do_cidadao.adapters.Download")
+    def test_one_argument(self, download):
+        fixture = get_fixture(OriginalIpca)
+        download.return_value.return_value.__enter__.return_value = fixture
+
         adpater = Ipca()
-        series = Series(("2020-01-1",))
-        self.assertEqual(
+        series = Series(("2006-07-01",))
+        self.assert_decimal_series_equal(
             adpater.adjust(series),
-            Series((Decimal(0.3123),)),
+            Series((Decimal(2.062688036971558),)),
         )

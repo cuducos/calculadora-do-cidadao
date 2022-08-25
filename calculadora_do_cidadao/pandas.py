1e7206da3cb37dbdd0dca18e2eade8b053423b55
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-08-24 20:31:12 -0400

    Adds all adapters to Pandas module

--- calculadora_do_cidadao/pandas.py
+++ calculadora_do_cidadao/pandas.py
@@ -1,4 +1,4 @@
-from calculadora_do_cidadao.adapters import ibge
+from calculadora_do_cidadao.adapters import cpi, dieese, ibge, igpm, selic
 
 
 class PandasWrapper:
@@ -20,8 +20,43 @@ class PandasWrapper:
         return wrapper
 
 
-# TODO: do this for all adapters
-Ipca = PandasWrapper.wrap(ibge.Ipca)
+AllUrbanCityAverage = PandasWrapper.wrap(cpi.AllUrbanCityAverage)
+CestaBasicaAracaju = PandasWrapper.wrap(dieese.CestaBasicaAracaju)
+CestaBasicaBelem = PandasWrapper.wrap(dieese.CestaBasicaBelem)
+CestaBasicaBeloHorizonte = PandasWrapper.wrap(dieese.CestaBasicaBeloHorizonte)
+CestaBasicaBoaVista = PandasWrapper.wrap(dieese.CestaBasicaBoaVista)
+CestaBasicaBrasilia = PandasWrapper.wrap(dieese.CestaBasicaBrasilia)
+CestaBasicaCampoGrande = PandasWrapper.wrap(dieese.CestaBasicaCampoGrande)
+CestaBasicaCentroOeste = PandasWrapper.wrap(dieese.CestaBasicaCentroOeste)
+CestaBasicaCuiaba = PandasWrapper.wrap(dieese.CestaBasicaCuiaba)
+CestaBasicaCuritiba = PandasWrapper.wrap(dieese.CestaBasicaCuritiba)
+CestaBasicaFlorianopolis = PandasWrapper.wrap(dieese.CestaBasicaFlorianopolis)
+CestaBasicaFortaleza = PandasWrapper.wrap(dieese.CestaBasicaFortaleza)
+CestaBasicaGoiania = PandasWrapper.wrap(dieese.CestaBasicaGoiania)
+CestaBasicaJoaoPessoa = PandasWrapper.wrap(dieese.CestaBasicaJoaoPessoa)
+CestaBasicaMacae = PandasWrapper.wrap(dieese.CestaBasicaMacae)
+CestaBasicaMacapa = PandasWrapper.wrap(dieese.CestaBasicaMacapa)
+CestaBasicaMaceio = PandasWrapper.wrap(dieese.CestaBasicaMaceio)
+CestaBasicaManaus = PandasWrapper.wrap(dieese.CestaBasicaManaus)
+CestaBasicaNatal = PandasWrapper.wrap(dieese.CestaBasicaNatal)
+CestaBasicaNordeste = PandasWrapper.wrap(dieese.CestaBasicaNordeste)
+CestaBasicaNorte = PandasWrapper.wrap(dieese.CestaBasicaNorte)
+CestaBasicaPalmas = PandasWrapper.wrap(dieese.CestaBasicaPalmas)
+CestaBasicaPortoAlegre = PandasWrapper.wrap(dieese.CestaBasicaPortoAlegre)
+CestaBasicaPortoVelho = PandasWrapper.wrap(dieese.CestaBasicaPortoVelho)
+CestaBasicaRecife = PandasWrapper.wrap(dieese.CestaBasicaRecife)
+CestaBasicaRioBranco = PandasWrapper.wrap(dieese.CestaBasicaRioBranco)
+CestaBasicaRioDeJaneiro = PandasWrapper.wrap(dieese.CestaBasicaRioDeJaneiro)
+CestaBasicaSalvador = PandasWrapper.wrap(dieese.CestaBasicaSalvador)
+CestaBasicaSaoLuis = PandasWrapper.wrap(dieese.CestaBasicaSaoLuis)
+CestaBasicaSaoPaulo = PandasWrapper.wrap(dieese.CestaBasicaSaoPaulo)
+CestaBasicaSudeste = PandasWrapper.wrap(dieese.CestaBasicaSudeste)
+CestaBasicaSul = PandasWrapper.wrap(dieese.CestaBasicaSul)
+CestaBasicaTeresina = PandasWrapper.wrap(dieese.CestaBasicaTeresina)
+CestaBasicaVitoria = PandasWrapper.wrap(dieese.CestaBasicaVitoria)
+Igpm = PandasWrapper.wrap(igpm.Igpm)
 Inpc = PandasWrapper.wrap(ibge.Inpc)
+Ipca = PandasWrapper.wrap(ibge.Ipca)
 Ipca15 = PandasWrapper.wrap(ibge.Ipca15)
 IpcaE = PandasWrapper.wrap(ibge.IpcaE)
+Selic = PandasWrapper.wrap(selic.Selic)
